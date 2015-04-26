e7f170f11d797114f7cbe6916c4d8f8dc7a5cb87
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-04-26 10:51:25 -0700

    added empty files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PWrapper',
+    py_modules=['PWrapper'],
+    version='0.0.1',
+    description='api wrapper',
+    author='Finley Hebert-Perkins',
+    url='https://github.com/BaySchoolCS2/ProjectWrapper',
+    install_requires=[
+        'requests>=1.1.0'
+    ]
